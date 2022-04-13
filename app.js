f88b2dd93058525d30b508dadbf9d15c66ba9fd7
Inseok Park <73049873+isp1106@users.noreply.github.com>
2022-04-13 21:38:54 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,38 @@
-const Container = document.getElementById('root');
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
-const NEWX_URL = 'https://api.hnpwa.com/v0/news/1.json';
-const CONTENT_URL = 'https://api.hnpwa.com/v0/item@id.json';
+const NEWS_URL ='https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-ajax.open('GET', NEWX_URL, false);
+ajax.open('GET', NEWS_URL, false);
 ajax.send();
 
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', function () {
-  //remove hash
-  const id = location.hash.substr(1);
+  const id = location.hash.substr(1)
 
   ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-  ajax.send();
+  ajax.send()
 
   const newsContent = JSON.parse(ajax.response);
   const title = document.createElement('h1');
-  title.innerHTML = newsContent.title;
-
+  title.innerHTML = `${ newsContent.title }`
   content.appendChild(title);
-
 });
 
 for (let i = 0; i < 10; i++) {
   const li = document.createElement('li');
   const a = document.createElement('a');
-  
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+  a.href = `#${ newsFeed[i].id }`;
+  a.innerHTML = `${ newsFeed[i].title } (${ newsFeed[i].comments_count })`;
+
 
   li.appendChild(a);
   ul.appendChild(li);
 }
 
-Container.appendChild(ul);
-Container.appendChild(content);
+container.appendChild(ul);
+container.appendChild(content);
