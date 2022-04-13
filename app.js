4baba77931d72dd6509e3f25b9f6435af3d88209
Inseok Park <73049873+isp1106@users.noreply.github.com>
2022-04-13 21:34:48 +0900

    Update app.js
    feat: add title, comment count at app.js

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
+const Container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
+const content = document.createElement('div');
 const NEWX_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item@id.json';
+
 ajax.open('GET', NEWX_URL, false);
 ajax.send();
 
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 
-console.log(newsFeed);
+window.addEventListener('hashchange', function () {
+  //remove hash
+  const id = location.hash.substr(1);
+
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  const title = document.createElement('h1');
+  title.innerHTML = newsContent.title;
+
+  content.appendChild(title);
+
+});
 
 for (let i = 0; i < 10; i++) {
   const li = document.createElement('li');
-  li.innerHTML = newsFeed[i].title;
+  const a = document.createElement('a');
+  
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
+Container.appendChild(ul);
+Container.appendChild(content);
